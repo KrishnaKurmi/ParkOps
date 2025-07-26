79b90fa0d2068ea2f5c7ed66004cd305264a9b46
Krishna Kurmi <hollybuddy.ts2206@gmail.com>
2025-07-26 21:24:01 +0530

    Add files via upload

---
+++ ParkOps/src/main/java/com/xansa/ParkOps/ParkOpsApplication.java
@@ -0,0 +1,13 @@
+package com.xansa.ParkOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkOpsApplication.class, args);
+	}
+
+}
---
+++ ParkOps/src/test/java/com/xansa/ParkOps/ParkOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xansa.ParkOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
