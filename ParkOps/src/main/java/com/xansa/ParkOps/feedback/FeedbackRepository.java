a2ee8144a075bc67078ff3c9f921fb546f30ec6c
Krishna Kurmi <hollybuddy.ts2206@gmail.com>
2025-07-29 15:12:37 +0530

    Add files via upload

---
+++ ParkOps/src/main/java/com/xansa/ParkOps/feedback/FeedbackRepository.java
@@ -0,0 +1,6 @@
+package com.xansa.ParkOps.feedback;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackRepository extends JpaRepository<Feedback,Integer> {
+}
